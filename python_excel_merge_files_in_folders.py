80fe3ecd79b9a1714343af5d02b9eaf48c126043
pgldev <nomail@*.*.com>
2019-07-31 14:41:12 +0200

    Adding the python file

--- python_excel_merge_files_in_folders.py
+++ python_excel_merge_files_in_folders.py
@@ -9,4 +9,6 @@ all_excel_files = []
 for f in list:
     all_excel_files.append(pd.read_excel(f))
 join = pd.concat(all_excel_files)
+# this till make the excel file. temp name is merge_01.xlsx
 join.to_excel("merge_01.xlsx")
+
