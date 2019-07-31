fc7c2700afa1c9189ee97243e8edff0c0c58a7f9
pgldev <nomail@*.*.com>
2019-07-31 14:54:54 +0200

    update added more comments to the python_excel_merge_files_in_folder.py

--- python_excel_merge_files_in_folders.py
+++ python_excel_merge_files_in_folders.py
@@ -4,10 +4,14 @@
 import pandas
 import glob
 
+# this list creates a list with the filepath
 list = glob.glob("*.xlsx")
+#list where we will append the files from the folder
 all_excel_files = []
+#loop for the appending itself
 for f in list:
     all_excel_files.append(pd.read_excel(f))
+#Merges the xlsx files from the all_excel_files with concat.
 join = pd.concat(all_excel_files)
 # this till make the excel file. temp name is merge_01.xlsx
 join.to_excel("merge_01.xlsx")
